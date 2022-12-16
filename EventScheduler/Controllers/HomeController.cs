12156681f5a497f0f479a502df86bee481feed9f
noori-Bhai <nooribhai35961@gmail.com>
2022-12-16 11:46:38 +0500

    Add project files.

---
+++ EventScheduler/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using EventScheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEventCalendar.Controllers
+{
+    public class HomeController : Controller 
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (MyEventShedulerEntities dc = new MyEventShedulerEntities())
+            {
+                dc.Configuration.LazyLoadingEnabled = false;
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            Thread.Sleep(3000);
+            var status = false;
+            using (MyEventShedulerEntities dc = new MyEventShedulerEntities())
+            {
+                if (e.EventId > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventId == e.EventId).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+
+                dc.SaveChanges();
+                status = true;
+
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (MyEventShedulerEntities dc = new MyEventShedulerEntities())
+            {
+                var v = dc.Events.Where(a => a.EventId == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
